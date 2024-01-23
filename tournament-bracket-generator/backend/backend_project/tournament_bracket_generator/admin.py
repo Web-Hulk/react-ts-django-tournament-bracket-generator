1a3d301a8988a74559e573f4ca5ee57ad160d9b7
IJ58LG <patryk.kwasek@ing.com>
2024-01-23 14:42:07 +0100

    Admin Panel: Added logic to not assign more than 4 people to one group and 1 player to more than 1 group

--- tournament-bracket-generator/backend/backend_project/tournament_bracket_generator/admin.py
+++ tournament-bracket-generator/backend/backend_project/tournament_bracket_generator/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from .models import Player, Tournament, Feedback, Fixture, GroupStage, KnockoutStage, RegistrationStatus
 
 class PlayerAdmin(admin.ModelAdmin):
@@ -15,6 +15,25 @@ class FixtureAdmin(admin.ModelAdmin):
 
 class GroupStageAdmin(admin.ModelAdmin):
   list_display = ('player', 'group_name', 'position', 'matches_played', 'wins', 'draws', 'loses', 'goals_for', 'goals_against', 'goals_difference', 'points', 'qualified')
+
+  def save_model(self, request, obj, form, change):
+    if GroupStage.objects.filter(group_name=obj.group_name).exclude(id=obj.id).count() >= 4:
+      messages.set_level(request, messages.ERROR)
+      messages.error(request, "Can't assign more than 4 people to one group")
+      return
+    
+    if GroupStage.objects.filter(player=obj.player).exclude(id=obj.id).exists():
+      messages.set_level(request, messages.ERROR)
+      messages.error(request, "Can't assign 1 player to more than 1 group")
+      return
+    
+    # Can 2 players or more can be on the same place in the group?
+    # if GroupStage.objects.filter(position=obj.position, group_name=obj.group_name).exists():
+    #   messages.set_level(request, messages.ERROR)
+    #   messages.error(request, "Can't have 2 players or more on the same place in the group")
+    #   return
+
+    super().save_model(request, obj, form, change)
 
 class KnockoutStageAdmin(admin.ModelAdmin):
   list_display = ('player_name', 'opponent_name', 'player_goals_1st_leg', 'opponent_goals_1st_leg', 'player_goals_2nd_leg', 'opponent_goals_2nd_leg', 'stage')
