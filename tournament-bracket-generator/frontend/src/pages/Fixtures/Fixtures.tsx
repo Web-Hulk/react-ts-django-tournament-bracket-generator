2f28ddd5f04ddfc131e2fa48508ab644c0fa855c
IJ58LG <patryk.kwasek@ing.com>
2024-02-17 19:30:55 +0100

    Frontend: Added filtering to return stages in correct order

--- tournament-bracket-generator/frontend/src/pages/Fixtures/Fixtures.tsx
+++ tournament-bracket-generator/frontend/src/pages/Fixtures/Fixtures.tsx
@@ -1,19 +1,35 @@
 import { Box } from "@mui/material";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { getData } from "../../api/axios";
 import { Fixture } from "../../components/Fixtures/Fixture";
 import { FixtureFilters } from "../../components/Fixtures/FixtureFilters";
 import { FixtureProps } from "../../types";
 import "./Fixtures.scss";
 
-const STAGE_ORDER = ["G", "QF", "SF", "3P", "F"];
+const STAGE_ORDER: string[] = ["G", "QF", "SF", "3P", "F"];
 
 export const Fixtures = () => {
   const [fixtures, setFixtures] = useState<FixtureProps[]>([]);
   const [filteredFixtures, setFilteredFixtures] = useState<FixtureProps[]>([]);
   const [stage, setStage] = useState<string>("");
 
-  useEffect(() => {
+  const filterFixturesByStage = useCallback(
+    (stage: string) => {
+      const filtered = stage
+        ? fixtures.filter((fixture) => fixture.stage === stage)
+        : fixtures;
+
+      filtered.sort(
+        (a, b) => STAGE_ORDER.indexOf(a.stage) - STAGE_ORDER.indexOf(b.stage)
+      );
+
+      setStage(stage);
+      setFilteredFixtures(filtered);
+    },
+    [fixtures]
+  );
+
+  const getFixtures = () => {
     getData("fixtures/")
       .then((response) => {
         console.log("Fixtures: ", response.data.results);
@@ -23,23 +39,15 @@ export const Fixtures = () => {
       .catch((error) => {
         console.log(error);
       });
+  };
+
+  useEffect(() => {
+    getFixtures();
   }, []);
 
-  const filterFixturesByStage = (stage: string) => {
-    const filtered = stage
-      ? fixtures.filter((fixture) => fixture.stage === stage)
-      : [...fixtures]; // create a new array to avoid mutating the original one
-
-    // Sort the fixtures based on the stage order
-    filtered.sort(
-      (a, b) => STAGE_ORDER.indexOf(a.stage) - STAGE_ORDER.indexOf(b.stage)
-    );
-
-    console.log("filtered: ", filtered);
-
-    setStage(stage);
-    setFilteredFixtures(filtered);
-  };
+  useEffect(() => {
+    filterFixturesByStage(stage);
+  }, [fixtures]);
 
   return (
     <Box className="fixtures-container">
